41a78ad3fea63b1c657cbbc7d06f41905598b258
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-02-21 10:15:33 -0300

    configuration of routes with mux

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,11 +1,26 @@
 package rotas
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Padrão de todas as rotas da API
 type Rotas struct {
-	URI string
-	Metodo string
-	Funcao func(http.ResponseWriter, *http.Request)
+	URI                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
 	RequerAutenticacao bool
+}
+
+// Insere todas as rotas no router
+func Configurar(r *mux.Router) *mux.Router {
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r
 }
